c88667a2322b3887f950d21d534014c03fe732ab
Erick Israel Meza Tineo <emezatin@emeal.nttdata.com>
2022-07-06 18:06:23 -0500

    actualizar link de azure

--- src/main/java/com/nttdata/productservices/application/feign/customer/CustomerClient.java
+++ src/main/java/com/nttdata/productservices/application/feign/customer/CustomerClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "customer-service", url = "localhost:8091/customers")
+@FeignClient(value = "customer-service", url = "https://customer-service-bootcamp.azurewebsites.net/customers")
 public interface CustomerClient {
 
     @GetMapping
